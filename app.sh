c96ee5f6b44416827110c673feb36543b5afa870
吴冰 <wubing@fuliaoyi.com>
2022-03-03 15:26:25 +0800

    sh

--- app.sh
+++ app.sh
@@ -55,7 +55,7 @@ time=$(date "+%m%d")
 test=Test
 #Android 打包
 echo '----- build android -----'
-flutter build apk
+flutter build apk --$type --obfuscate --split-debug-info=xx --build-name=$versionName --build-number=$versionCode
 #执行 iOS 打包
 #flutter build ipa --export-options-plist=./debug_export_options/ExportOptions.plist
 #修改包名称
