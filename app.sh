ca90e37d6619bb20e8d6d89114e5569707d4098c
吴冰 <wubing@fuliaoyi.com>
2022-03-03 15:14:16 +0800

    sh

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ilex
 
 #获取Git SHA1
-GIT_SHA1=`(git show-ref --head --hash=8 2> /dev/null || echo 00000000) | head -n1`
+#GIT_SHA1=`(git show-ref --head --hash=8 2> /dev/null || echo 00000000) | head -n1`
 #默认打包类型
 DEFAULT_BUILD_TYPE=release
 
@@ -63,6 +63,6 @@ test=Test
 #Android 打包
 flutter build apk --$type --obfuscate --split-debug-info=xx --build-name=$versionName --build-number=$versionCode
 #执行 iOS 打包
-flutter build ipa --export-options-plist=./debug_export_options/ExportOptions.plist
+#flutter build ipa --export-options-plist=./debug_export_options/ExportOptions.plist
 #修改包名称
-mv "build/app/outputs/flutter-apk/app-${type}.apk" "build/app/outputs/flutter-apk/hn_lottery_${versionName}+${versionCode}_${GIT_SHA1}_${type}.apk"
+#mv "build/app/outputs/flutter-apk/app-${type}.apk" "build/app/outputs/flutter-apk/hn_lottery_${versionName}+${versionCode}_${GIT_SHA1}_${type}.apk"
