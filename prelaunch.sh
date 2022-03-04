0e32d81d9509fa944dea07987c497a4ae619ac1e
吴冰 <wubing@fuliaoyi.com>
2022-03-04 11:51:02 +0800

    sh

--- prelaunch.sh
+++ prelaunch.sh
@@ -1,10 +1,14 @@
-echo 'run----prelaunch.sh-------'
 #!/bin/sh
 echo 'run1----prelaunch.sh-------'
 #获取Git SHA1
 GIT_SHA1=`(git show-ref --head --hash=8 2> /dev/null || echo 00000000) | head -n1`
 #默认打包类型
 DEFAULT_BUILD_TYPE=release
+if [ ! $buildType ]; then
+type=$DEFAULT_BUILD_TYPE
+  else
+type=$buildType
+fi
 echo 'run2----prelaunch.sh-------'
 #获取版本号
 versionCode=""
@@ -12,9 +16,27 @@ version=""
 versionCodeSpe=""
 pubspecFile="pubspec.yaml"
 echo 'run2.5----prelaunch.sh-------'
-
-echo 'pubspecFile-------'
-
+if [ -e $pubspecFile ]; then
+    echo 'pubspecFile存在'
+fi
+if [ -e $pubspecFile ]
+  then
+      if [ -r $pubspecFile ]
+        then
+          if [ -w $pubspecFile ]
+            then
+              #修改文件
+              #webview
+              version=`sed -n '18p'  $pubspecFile`
+          else
+            echo "文件不可写"
+          fi
+      else
+         echo "文件不可读"
+      fi
+else
+   echo "文件不存在"
+fi
 echo 'run3----prelaunch.sh-------'
 
 #对IFS变量 进行替换处理 拆分分号
@@ -30,10 +52,12 @@ IFS="$OLD_IFS"
 versionName=${array[0]}
 #versionCode=`expr "${versionName}"|sed "s/\.*//g"`
 versionCode=`expr "${array[1]}"|sed "s/\r//g"`
-echo 'run3.5----prelaunch.sh-------'
-
 #指定版本号
-
+if [ ! $buildVersionName ]; then
+  echo "没有指定版本，默认使用配置文件的版本"
+else
+  versionName=$buildVersionName
+fi
 
 time=$(date "+%m%d")
 test=Test
