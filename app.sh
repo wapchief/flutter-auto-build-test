74bd87625e3fb5fc131e654a6a6d695873e371b0
吴冰 <wubing@fuliaoyi.com>
2022-03-04 17:56:20 +0800

    jenkinsFile

--- app.sh
+++ app.sh
@@ -1,15 +1,12 @@
-#!/bin/bash -i1
-#获取Git SHA1
+#!/bin/bash -ilex
+#Git SHA1
 GIT_SHA1=`(git show-ref --head --hash=8 2> /dev/null || echo 00000000) | head -n1`
-#默认打包类型
 DEFAULT_BUILD_TYPE=release
-#打包类型
 if [ ! $buildType ]; then
 type=$DEFAULT_BUILD_TYPE
   else
 type=$buildType
 fi
-#获取版本号
 versionCode=""
 version=""
 versionCodeSpe=""
@@ -20,24 +17,21 @@ if [ -e $pubspecFile ]
         then
           if [ -w $pubspecFile ]
             then
-              #修改文件
               #webview
               version=`sed -n '18p'  $pubspecFile`
           else
-            echo "文件不可写"
+            echo "no -w"
           fi
       else
-         echo "文件不可读"
+         echo "no -r"
       fi
 else
-   echo "文件不存在"
+   echo "no -e"
 fi
-#对IFS变量 进行替换处理 拆分分号
 OLD_IFS="$IFS"
 IFS=": "
 array=($version)
 IFS="$OLD_IFS"
-#拆分加号
 OLD_IFS="$IFS"
 IFS="+"
 array=(${array[1]})
@@ -45,20 +39,17 @@ IFS="$OLD_IFS"
 versionName=${array[0]}
 #versionCode=`expr "${versionName}"|sed "s/\.*//g"`
 versionCode=`expr "${array[1]}"|sed "s/\r//g"`
-#指定版本号
 if [ ! $buildVersionName ]; then
-  echo "没有指定版本，默认使用配置文件的版本"
+  echo "no version code"
 else
   versionName=$buildVersionName
 fi
 time=$(date "+%m%d")
 test=Test
-#Android 打包
+#Android
 flutter build apk --$type --obfuscate --split-debug-info=xx --build-name=$versionName --build-number=$versionCode
-#执行 iOS 打包
+#iOS
 #flutter build ipa --export-options-plist=./debug_export_options/ExportOptions.plist
-#复制文件
 cp -rf "build/app/outputs/flutter-apk/app-${type}.apk" build/app/
-#重命名
 apkNewName=flutter_build_${versionName}+${versionCode}_${GIT_SHA1}_${type}
 mv "build/app/app-${type}.apk" "build/app/${apkNewName}.apk"
